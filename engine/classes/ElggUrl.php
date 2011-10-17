4a388a184ba41820cec52bd09b0fdc6ef17d27b3
Evan Winslow <evan@elgg.org>
2011-10-17 02:41:27 -0700

    Defines ElggUrl skeleton

---
+++ engine/classes/ElggUrl.php
@@ -0,0 +1,26 @@
+<?php
+class ElggUrl {
+	public function __construct($url) {
+		
+	}
+	
+	public function equals($url) {
+		
+	}
+	
+	public function addActionTokens() {
+		
+	}
+	
+	public function removeQueryElement() {
+		
+	}
+	
+	public function setParameter($param, $value) {
+		
+	}
+	
+	public function __toString() {
+		
+	}
+}
