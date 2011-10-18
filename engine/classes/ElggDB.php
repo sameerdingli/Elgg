691e7f9ae7b22ea1482a7686956ee6d785d07160
Evan Winslow <evan@elgg.org>
2011-10-17 22:34:24 -0700

    Implements get/set/unset config functions (untested)

--- engine/classes/ElggDB.php
+++ engine/classes/ElggDB.php
@@ -93,15 +93,65 @@ class ElggDB {
 	}
 	
 	public function getConfig($name, ElggSite $site) {
+		$name = $this->escapeString($name);
 		
+		// check for deprecated values.
+		// @todo might be a better spot to define this?
+		$new_name = false;
+		switch($name) {
+			case 'viewpath':
+				$new_name = 'view_path';
+				$dep_version = 1.8;
+				break;
+		
+			case 'pluginspath':
+				$new_name = 'plugins_path';
+				$dep_version = 1.8;
+				break;
+		
+			case 'sitename':
+				$new_name = 'site_name';
+				$dep_version = 1.8;
+				break;
+		}
+		
+		// show dep message
+		if ($new_name) {
+			$name = $new_name;
+			$this->logger->deprecatedNotice($msg, $dep_version);
+		}
+		
+		$result = $this->getDataRow("SELECT value FROM {$this->prefix}config
+				WHERE name = '$name' and site_guid = $site->guid");
+		
+		return $result ? unserialize($result->value) : null;
 	}
 	
 	public function setConfig($name, $value, ElggSite $site) {
+		$name = trim($name);
 		
+		// cannot store anything longer than 32 characters in db, so catch before we set
+		if (elgg_strlen($name) > 32) {
+			$this->logger->log("The name length for configuration variables cannot be greater than 32", "ERROR");
+			return false;
+		}
+		
+		// Unset existing
+		$this->unsetConfig($name, $site);
+		
+		$value = $this->escapeString(serialize($value));
+		
+		$query = "insert into {$this->prefix}config"
+		. " set name = '$name', value = '$value', site_guid = $site->guid";
+
+		return $this->insertData($query) !== false;		
 	}
 	
 	public function unsetConfig($name, ElggSite $site) {
+		$name = $this->escapeString($name);
 		
+		$query = "delete from {$this->prefix}config where name = '$name' and site_guid = $site->guid";
+		return $this->deleteData($query);
 	}	
 	
 	private static $instance;
