bee089f41f29f9e2dcd9a1780aa916ee2b79e15f
Evan Winslow <evan@elgg.org>
2011-11-16 22:42:02 -0800

    elgg/string and test now working

--- views/default/js/elgg/string.js
+++ views/default/js/elgg/string.js
@@ -1,23 +1,28 @@
-/**
- * Instead of overriding the native String prototype, provide
- * some "static" helper functions.
- */
-elgg.provide('elgg.string');
-
-/**
- * Removes a character from the left side of a string.
- * 
- * @param {String} str The string to trim
- * @param {String} ch  The character to remove
- * 
- * @return {String}
- */
-elgg.string.ltrim = function(str, ch) {
-	if (str.ltrim) {
-		return str.ltrim(ch);
-	} else if (str.indexOf(ch) === 0) {
-		return str.substring(ch.length);
-	} else {
-		return str;
-	}
-};
+define('elgg/string', ['elgg'], function(elgg) {
+	/**
+	 * Instead of overriding the native String prototype, provide
+	 * some "static" helper functions.
+	 */
+	var string = elgg.provide('elgg.string');
+	
+	/**
+	 * Removes whitespace from the left side of a string.
+	 * 
+	 * @param {String} str The string to trim
+	 * 
+	 * @return {String} The new string with left whitespace removed.
+	 */
+	string.ltrim = function(str) {
+		return str.replace(/^\s+/, "");
+	};
+	
+	string.rtrim = function(str) {
+		return str.replace(/\s+$/, "");
+	};
+	
+	string.trim = function(str) {
+		return string.ltrim(string.rtrim(str));
+	};
+	
+	return string;
+});
---
+++ views/default/js/elgg/stringTest.js
@@ -0,0 +1,17 @@
+define('elgg/stringTest', ['elgg/string'], function(string) {
+	var Test = TestCase('elgg/stringTest');
+	
+	Test.prototype.testRtrim = function() {
+		assertEquals(" r", string.rtrim(" r "));
+	};
+	
+	Test.prototype.testLtrim = function() {
+		assertEquals("l ", string.ltrim(" l "));
+	};
+	
+	Test.prototype.testTrim = function() {
+		assertEquals("t", string.trim(" t "));
+	};
+	
+	return Test;
+});
